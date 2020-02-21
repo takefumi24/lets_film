73e239588c2c94de2dcb6f14d0c09d58f87a0d3b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-21 15:54:41 +0900

    continue

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_113402) do
+ActiveRecord::Schema.define(version: 2020_02_21_062634) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "feelings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
