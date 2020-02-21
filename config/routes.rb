fa5e5370f816e56034a10d4e942bc70a1c816b7b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-21 16:56:41 +0900

    create_films-table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "feelings#index"
+  resources :films
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_062634) do
+ActiveRecord::Schema.define(version: 2020_02_21_073032) do
 
   create_table "feelings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "films", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.bigint "user_id"
+    t.bigint "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_films_on_review_id"
+    t.index ["user_id"], name: "index_films_on_user_id"
   end
 
 end
