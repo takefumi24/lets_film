3da51413eb68a71f09dff4639b36efddc5a8f002
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-22 18:33:08 +0900

    create_films-index-view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_082902) do
+ActiveRecord::Schema.define(version: 2020_02_21_113531) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_082902) do
     t.bigint "review_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["review_id"], name: "index_films_on_review_id"
     t.index ["user_id"], name: "index_films_on_user_id"
   end
@@ -48,6 +49,18 @@ ActiveRecord::Schema.define(version: 2020_02_21_082902) do
     t.index ["feeling_id"], name: "index_reviews_on_feeling_id"
     t.index ["film_id"], name: "index_reviews_on_film_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
