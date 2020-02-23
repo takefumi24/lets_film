443e32a700a691edfa52b1deb6258edcbb158d8a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-23 14:02:29 +0900

    add_controller

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,2 +1,15 @@
 class FilmsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @films = Film.new
+  end
+
+  def show
+    @film = Film.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
