ffc9f1188fa286c5c3f5aeec813768415f1726e2
takefumi24 <t_ohba24@yahoo.co.jp>
2020-02-26 16:16:22 +0900

    add_検索バー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "feelings#index"
+  namespace :films do
+    resources :searches, only: :index
+  end
   resources :films do
     resources :reviews do
       resources :comments
